200cc2457283835e3add872a23217fc315827d6d
Roger Lew <rogerlew@gmail.com>
2025-05-11 21:57:02 -0700

    give up on service

---
+++ run_api.bat
@@ -0,0 +1,5 @@
+@echo off
+set SESSIONNAME=Console
+set USERPROFILE=C:\Users\install
+cd /d C:\ClimateNA_v7.50\api
+C:\Users\install\miniconda3\envs\climateBC-NA\python.exe C:\ClimateNA_v7.50\api\main.py
